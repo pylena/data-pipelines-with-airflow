1fe61f3f984c0b4c40e5c9e36445737bb09635c6
Lina Alsehli <141145520+pylena@users.noreply.github.com>
2025-06-26 20:53:04 +0300

    Update load

--- plugins/operators/load_dimension.py
+++ plugins/operators/load_dimension.py
@@ -8,15 +8,29 @@ class LoadDimensionOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                conn_id = "",
+                table = "",
+                sql_statement = "",
+                append_only = "",
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        # Map params 
+        self.conn_id = conn_id,
+        self.table = table
+        self.sql_statement = sql_statement
+        self.append_only = append_only            
 
-    def execute(self, context):
-        self.log.info('LoadDimensionOperator not implemented yet')
+  def execute(self, context):
+        self.log.info('Get Credentials')
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        # remove data from table
+        if not self.append_only:
+            self.log.info(f"Remove data from Redshift table {self.table}")
+            redshift.run("DELETE FROM {}".format(self.table))
+
+        self.log.info(f"Start Load data into fact table in Redshift {self.table}")
+        formatted_sql = LoadDimensionOperator.sql_insert.format(self.table, self.sql_statement)
+
+        redshift.run(formatted_sql)
